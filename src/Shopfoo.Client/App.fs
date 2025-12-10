c9e3ef8eb986fc8670ec8e11a36f1ea580ab4294
Romain Deneau <rdeneau@d-edge.com>
2025-12-10 17:04:39 +0100

    chore: ✨ init from SAFEr template

---
+++ src/Shopfoo.Client/App.fs
@@ -0,0 +1,5 @@
+module Shopfoo.Client.App
+
+open Feliz
+
+ReactDOM.createRoot(Browser.Dom.document.getElementById("safer-app")).render(View.AppView())
---
+++ vite.config.js
@@ -0,0 +1,23 @@
+﻿import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from "@tailwindcss/vite";
+
+/** @type {import('vite').UserConfig} */
+export default defineConfig({
+    plugins: [react({ jsxRuntime: 'classic'}),tailwindcss()], // jsxRuntime: 'classic' is required for fast-refresh for .js files
+    root: "./src/Shopfoo.Client",
+    server: {
+        port: 8080,
+        proxy: {
+            '/api': 'http://localhost:5000',
+        },
+        watch: {
+            ignored: [
+                "**/*.fs" // Don't watch F# files
+            ]
+        }
+    },
+    build: {
+        outDir:"../../publish/app/public"
+    }
+})
