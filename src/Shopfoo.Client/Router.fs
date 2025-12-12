1409e128b09698a30491dc53fd194cf6429aefd5
Romain Deneau <rdeneau@d-edge.com>
2025-12-12 07:51:28 +0100

    refactor: ♻️ use PageUrl in navigatePage, clearer than noQueryString

--- src/Shopfoo.Client/Router.fs
+++ src/Shopfoo.Client/Router.fs
@@ -4,9 +4,23 @@ open Browser.Types
 open Feliz.Router
 open Fable.Core.JsInterop
 
+type private PageUrl = {
+    Segments: string list
+    Query: (string * string) list
+} with
+    static member WithSegments([<System.ParamArray>] segments) = {
+        Segments = List.ofArray segments // ↩
+        Query = []
+    }
+
+    static member Root = PageUrl.WithSegments()
+
+    member this.WithQueryParam(name: string, value: string) = // ↩
+        { this with Query = (name, value) :: this.Query }
+
 type Page =
-    | Index
     | About
+    | Index
 
     member this.Key =
         match this with
@@ -23,26 +37,22 @@ module Page =
         | [] -> Page.Index
         | _ -> defaultPage
 
-    let noQueryString segments : string list * (string * string) list = segments, []
-
-    let toUrlSegments =
-        function
-        | Page.Index -> [] |> noQueryString
-        | Page.About -> [ "about" ] |> noQueryString
+let private (|PageUrl|) =
+    function
+    | Page.About -> PageUrl.WithSegments("about")
+    | Page.Index -> PageUrl.Root
 
 [<RequireQualifiedAccess>]
 module Router =
     let goToUrl (e: MouseEvent) =
         e.preventDefault ()
-
         let href: string = !!e.currentTarget?attributes?href?value
-
         Router.navigatePath href
 
-    let navigatePage (p: Page) =
-        p |> Page.toUrlSegments |> Router.navigatePath
+    let navigatePage (PageUrl pageUrl) =
+        Router.navigatePath (pageUrl.Segments, queryString = pageUrl.Query)
 
 [<RequireQualifiedAccess>]
 module Cmd =
-    let navigatePage (p: Page) =
-        p |> Page.toUrlSegments |> Cmd.navigatePath
+    let navigatePage (PageUrl pageUrl) =
+        Cmd.navigatePath (pageUrl.Segments, queryString = pageUrl.Query)
