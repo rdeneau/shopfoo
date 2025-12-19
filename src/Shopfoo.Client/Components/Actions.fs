8245f169fee0392351d26baa04e19c3b0fa78863
Romain Deneau <rdeneau@d-edge.com>
2025-12-19 17:42:33 +0100

    refactor: ♻️ extract Components/Actions from Pages/Product/Actions

---
+++ src/Shopfoo.Client/Components/Actions.fs
@@ -0,0 +1,126 @@
+﻿module Shopfoo.Client.Components.Actions
+
+open Feliz
+open Feliz.DaisyUI
+open Shopfoo.Domain.Types
+open Shopfoo.Domain.Types.Security
+
+type Action = {
+    Key: string
+    Text: string
+    OnClick: unit -> unit
+} with
+    static member Emoji key (emoji, text) onClick : Action = {
+        Key = key
+        Text = $"%s{emoji}  {text}"
+        OnClick = onClick
+    }
+
+[<RequireQualifiedAccess>]
+type Symbol =
+    | None
+    | Left of string
+    | Right of string
+
+type Value =
+    | Natural of value: int
+    | Money of value: decimal option * currency: Symbol
+
+    static member OfMoney =
+        function
+        | Dollars value -> Money(Some value, Symbol.Left "$")
+        | Euros value -> Money(Some value, Symbol.Right "€")
+
+    static member OfMoneyOptional =
+        function
+        | Some money -> Value.OfMoney money
+        | None -> Money(None, Symbol.None)
+
+    member this.Symbol =
+        match this with
+        | Value.Natural _ -> Symbol.None
+        | Value.Money(_, symbol) -> symbol
+
+    member this.Text =
+        match this with
+        | Value.Natural n -> $"%i{n}"
+        | Value.Money(None, _) -> ""
+        | Value.Money(Some f, _) -> $"%0.2f{f}"
+
+[<ReactComponent>]
+let ActionsDropdown key access (value: Value) (actions: Action list) =
+    let itemElement (action: Action) =
+        Html.li [
+            prop.key $"{key}-action--{action.Key}"
+            prop.children [
+                Html.a [
+                    prop.key $"{key}-action--{action.Key}--link"
+                    prop.text action.Text
+                    prop.className "whitespace-nowrap"
+                    prop.onClick (fun _ -> action.OnClick())
+                ]
+            ]
+        ]
+
+    Html.div [
+        prop.key $"%s{key}-div"
+        prop.className "flex items-center mb-4 w-full"
+        prop.children [
+            Daisy.label.input [
+                prop.key $"{key}-label-input"
+                prop.className "bg-base-300 flex-1"
+                prop.children [
+                    match value.Symbol with
+                    | Symbol.Left symbol -> Daisy.label [ prop.key $"{key}-label-symbol"; prop.text symbol ]
+                    | _ -> ()
+
+                    Html.input [
+                        prop.key $"{key}-input"
+                        prop.className "flex-1"
+                        prop.defaultValue value.Text
+                        prop.readOnly true
+                        prop.type' "text"
+                    ]
+
+                    match value.Symbol with
+                    | Symbol.Right symbol -> Daisy.label [ prop.key $"{key}-label-symbol"; prop.text symbol ]
+                    | _ -> ()
+                ]
+            ]
+
+            match actions, access with
+            | [], _
+            | _, None
+            | _, Some Access.View -> ()
+            | _, Some Access.Edit ->
+                Daisy.dropdown [
+                    dropdown.hover
+                    dropdown.end'
+                    prop.key $"{key}-dropdown"
+                    prop.className "ml-2"
+                    prop.children [
+                        Daisy.button.button [ // ↩
+                            button.primary
+                            button.outline
+                            prop.key $"{key}-dropdown-button"
+                            prop.className "p-3"
+                            prop.text "⏷"
+                        ]
+                        Daisy.dropdownContent [
+                            prop.key $"{key}-dropdown-content"
+                            prop.className "p-2 shadow menu bg-base-100 rounded-box"
+                            prop.tabIndex 0
+                            prop.children [
+                                Html.ul [
+                                    prop.key $"{key}-dropdown-list"
+                                    prop.children [
+                                        for action in actions do
+                                            itemElement action
+                                    ]
+                                ]
+                            ]
+                        ]
+                    ]
+                ]
+        ]
+    ]
--- src/Shopfoo.Client/Pages/Product/Actions.fs
+++ src/Shopfoo.Client/Pages/Product/Actions.fs
@@ -5,6 +5,7 @@ open Feliz
 open Feliz.DaisyUI
 open Feliz.UseElmish
 open Shopfoo.Client.Components
+open Shopfoo.Client.Components.Actions
 open Shopfoo.Client.Remoting
 open Shopfoo.Domain.Types
 open Shopfoo.Domain.Types.Sales
@@ -33,125 +34,7 @@ let private update (fullContext: FullContext) (msg: Msg) (model: Model) =
     | PricesFetched(Ok response) -> { model with Prices = response.Prices |> Remote.ofOption }, Cmd.none
     | PricesFetched(Error apiError) -> { model with Prices = Remote.LoadError apiError }, Cmd.none
 
-// -- View ----
-
-type private Action = {
-    Key: string
-    Text: string
-    OnClick: unit -> unit
-} with
-    static member Emoji(emoji, key, text, onClick: unit -> unit) : Action = {
-        Key = key
-        Text = $"%s{emoji}  {text}"
-        OnClick = onClick
-    }
-
-[<RequireQualifiedAccess>]
-type private Symbol =
-    | None
-    | Left of string
-    | Right of string
-
-type private Value =
-    | Natural of value: int
-    | Money of value: decimal * currency: Symbol
-
-    static member OfMoney =
-        function
-        | Dollars value -> Money(value, Symbol.Left "$")
-        | Euros value -> Money(value, Symbol.Right "€")
-
-    member this.Symbol =
-        match this with
-        | Value.Natural _ -> Symbol.None
-        | Value.Money(_, symbol) -> symbol
-
-    member this.Text =
-        match this with
-        | Value.Natural value -> $"%i{value}"
-        | Value.Money(value, _) -> $"%0.2f{value}"
-
 [<ReactComponent>]
-let private ActionsDropdown key access (value: Value) (actions: Action list) =
-    let itemElement (action: Action) =
-        Html.li [
-            prop.key $"{key}-action--{action.Key}"
-            prop.children [
-                Html.a [
-                    prop.key $"{key}-action--{action.Key}--link"
-                    prop.text action.Text
-                    prop.className "whitespace-nowrap"
-                    prop.onClick (fun _ -> action.OnClick())
-                ]
-            ]
-        ]
-
-    Html.div [
-        prop.key $"%s{key}-div"
-        prop.className "flex items-center mb-4 w-full"
-        prop.children [
-            Daisy.label.input [
-                prop.key $"{key}-label-input"
-                prop.className "bg-base-300 flex-1"
-                prop.children [
-                    match value.Symbol with
-                    | Symbol.Left symbol -> Daisy.label [ prop.key $"{key}-label-symbol"; prop.text symbol ]
-                    | _ -> ()
-
-                    Html.input [
-                        prop.key $"{key}-input"
-                        prop.className "flex-1"
-                        prop.defaultValue value.Text
-                        prop.readOnly true
-                        prop.type' "text"
-                    ]
-
-                    match value.Symbol with
-                    | Symbol.Right symbol -> Daisy.label [ prop.key $"{key}-label-symbol"; prop.text symbol ]
-                    | _ -> ()
-                ]
-            ]
-
-            match access with
-            | Some Access.Edit ->
-                Daisy.dropdown [
-                    dropdown.hover
-                    dropdown.end'
-                    prop.key $"{key}-dropdown"
-                    prop.className "ml-2"
-                    prop.children [
-                        Daisy.button.button [ // ↩
-                            button.primary
-                            button.outline
-                            prop.key $"{key}-dropdown-button"
-                            prop.className "p-3"
-                            prop.text "⏷"
-                        ]
-                        Daisy.dropdownContent [
-                            prop.key $"{key}-dropdown-content"
-                            prop.className "p-2 shadow menu bg-base-100 rounded-box"
-                            prop.tabIndex 0
-                            prop.children [
-                                Html.ul [
-                                    prop.key $"{key}-dropdown-list"
-                                    prop.children [
-                                        for action in actions do
-                                            itemElement action
-                                    ]
-                                ]
-                            ]
-                        ]
-                    ]
-                ]
-            | _ -> ()
-        ]
-    ]
-
-[<ReactComponent>]
-let ActionsForm key fullContext sku =
-    let model, dispatch =
-        React.useElmish (init fullContext sku, update fullContext, [||])
-
     let translations = fullContext.Translations
 
     React.fragment [
